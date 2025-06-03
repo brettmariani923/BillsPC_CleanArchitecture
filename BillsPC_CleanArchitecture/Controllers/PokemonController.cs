da87e323d73f339ffb7acafc99e5017796114053
Brett Mariani <brettmariani923@gmail.com>
2025-06-03 00:33:34 -0500

    fixed typo

--- BillsPC_CleanArchitecture/Controllers/PokemonController.cs
+++ BillsPC_CleanArchitecture/Controllers/PokemonController.cs
@@ -9,7 +9,7 @@ namespace BillsPC_CleanArchitecture.Api.Controllers
     //is your entrypoint, it accepts requests and returns ressponses. Go to PokemonService.cs in the Application layer next for the next step in the process.
 
     //Part 2 (dont read until after completing the steps): This is where the results are returned to the view for the user to see. It takes that information we worked so hard to get from different layers and returns it to the view for the user to see.
-    // So to break it down *  var allPokemon = await _pokemonService.GetAllPokemonWithImagesAsync();  * gets the data from the service layer, which is where we are now after the previous steps.
+    // So to break it down *  var allPokemon = await _pokemonService.GetAllPokemonWithImagesAsync();  * gets the data from the service layer, which is where we were during the previous step.
     // Once it has the information it needs, it returns it to the view using *  return View("ReturnAllPokemon", allPokemon);  *. This directs the information to the "ReturnAllPokemon" view, which is where the user will see the results of their request.
     // Great job! You've made it through the entire process of how the data flows from the user request to the final view. If you have any questions about this or any other part of the code, feel free to ask me on discord and we can go over it together.
     // If you are curious, look at the ReturnAllPokemon.cshtml file in the Views/Pokemon folder to see how the data is displayed on the webpage.
